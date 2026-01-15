bc3dcbf607d19e1f8ef20497c0b279568c7d4d7d
Rafael Trindade <rafatrindade.exe@gmail.com>
2026-01-15 12:00:42 -0300

    feat(scripts): adiciona scripts de profilling (silver)

--- scripts/profiling/silver/profile_telco.py
+++ scripts/profiling/silver/profile_telco.py
@@ -55,7 +55,7 @@ md_file = init_md_report(
 # %% 
 # GARANTIA DE UNICIDADE E QUALIDADE #################
 #####################################################
-chave_tecnica_cols = ["num_cpf", "safra", "prod", "flag_instalacao"]
+chave_tecnica_cols = ["num_cpf", "safra", "prod"]
 
 md = "### 🔑 Garantia de Unicidade: `silver/telco`\n"
 md += f"- **Chave Técnica:** `{', '.join(chave_tecnica_cols)}`\n"
