81cf39bba7cb595023b4adad464fca6e8b9098aa
Rafael Trindade <rafatrindade.exe@gmail.com>
2026-01-23 23:28:10 -0300

    feat(scripts): adiciona scripts de profilling (silver)

--- scripts/profiling/silver/profile_recarga.py
+++ scripts/profiling/silver/profile_recarga.py
@@ -4,11 +4,7 @@
 from pathlib import Path
 import sys
 
-PROJECT_ROOT = Path().resolve()
-while PROJECT_ROOT.name != "scripts":
-    PROJECT_ROOT = PROJECT_ROOT.parent
-PROJECT_ROOT = PROJECT_ROOT.parent 
-
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
 sys.path.insert(0, str(PROJECT_ROOT))
 
 
